5f85ac763b3edef1b3f8818084a0dc0a67d56d7d
Neta Shoham <shohamne@gmail.com>
2018-05-19 13:51:41 +0300

    add mnist batch

---
+++ batch_train_mnist-1.py
@@ -0,0 +1,45 @@
+from os import path
+import train
+import csv
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+from multiprocessing import Pool, cpu_count
+
+train.args.no_cuda = True
+train.args.epochs = 500
+train.args.result_file = path.join('results',path.basename(__file__))
+train.args.data_set = 'mnist'
+
+with open(train.args.result_file, 'w') as fp:
+    writer = csv.DictWriter(fp, train.results.keys())
+    writer.writeheader()
+
+
+
+tasks = []
+for seed in range(100,105):
+    for set_size_range in [[10,100], [100,1000]]:
+        for max_set_new_size_train in set_size_range:
+            for max_set_new_size_test in set_size_range:
+                for new_sets_number_train in [int(np.ceil(float(set_size_range[1])/max_set_new_size_train))]:
+                    for new_sets_number_test in [int(np.ceil(float(set_size_range[1]) / max_set_new_size_test))]:
+                        new_args = deepcopy(train.args)
+                        new_args.max_set_new_size_train = max_set_new_size_train
+                        new_args.new_sets_number_train = new_sets_number_train
+                        new_args.max_set_new_size_test = max_set_new_size_test
+                        new_args.new_sets_number_test = new_sets_number_test
+                        new_args.set_size_range = set_size_range
+                        new_args.seed = seed
+                        tasks.append(new_args)
+
+pool = Pool(cpu_count())
+#pool = Pool(1)
+
+result = pool.map(train.main, tasks)
+result_df = pd.DataFrame(result)
+pool.close()
+pool.join()
+
+result_df.to_csv('{}.csv'.format(train.args.result_file))
+
---
+++ batch_train_rotations-1.py
@@ -0,0 +1,42 @@
+from os import path
+import train
+import csv
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+from multiprocessing import Pool, cpu_count
+
+train.args.no_cuda = True
+train.args.epochs = 500
+train.args.result_file = path.join('/tmp',path.basename(__file__))
+
+with open(train.args.result_file, 'w') as fp:
+    writer = csv.DictWriter(fp, train.results.keys())
+    writer.writeheader()
+
+
+
+tasks = []
+for seed in range(100,105):
+    for max_set_new_size_train in [10, 100]:
+        for max_set_new_size_test in [10, 100]:
+            for new_sets_number_train in [int(np.ceil(100.0/max_set_new_size_train))]:
+                for new_sets_number_test in [int(np.ceil(100.0 / max_set_new_size_test))]:
+                    new_args = deepcopy(train.args)
+                    new_args.max_set_new_size_train = max_set_new_size_train
+                    new_args.new_sets_number_train = new_sets_number_train
+                    new_args.max_set_new_size_test = max_set_new_size_test
+                    new_args.new_sets_number_test = new_sets_number_test
+                    new_args.seed = seed
+                    tasks.append(new_args)
+
+pool = Pool(cpu_count())
+#pool = Pool(1)
+
+result = pool.map(train.main, tasks)
+result_df = pd.DataFrame(result)
+pool.close()
+pool.join()
+
+result_df.to_csv('{}.csv'.format(train.args.result_file))
+
