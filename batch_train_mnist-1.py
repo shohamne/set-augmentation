da582a605afa4c78410b3a57967917feb55e0af0
Neta Shoham <neta.shoham@pixoneye.com>
2018-05-20 15:52:35 +0300

    add ID

--- batch_train_mnist-1.py
+++ batch_train_mnist-1.py
@@ -22,12 +22,15 @@ with open(train.args.result_file, 'w') as fp:
     writer.writeheader()
 
 tasks = []
+id = 0
 for seed in range(100,105):
     for set_size_range in [[10,100], [100,1000]]:
         for max_set_new_size_train in set_size_range:
             for max_set_new_size_test in set_size_range:
                 for new_sets_number_train in [int(np.ceil(float(set_size_range[1])/max_set_new_size_train))]:
                     for new_sets_number_test in [int(np.ceil(float(set_size_range[1]) / max_set_new_size_test))]:
+                        id += 1
+                        new_args.id = id
                         new_args = deepcopy(train.args)
                         new_args.max_set_new_size_train = max_set_new_size_train
                         new_args.new_sets_number_train = new_sets_number_train
