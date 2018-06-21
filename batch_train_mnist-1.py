1511bb1ff91e35ac3de4997d257d3c8dd1352ea9
Neta Shoham <shohamne@gmail.com>
2018-06-21 15:05:44 +0300

    synchronize batch_train_mnist-1.py with train.py

--- batch_train_mnist-1.py
+++ batch_train_mnist-1.py
@@ -1,6 +1,5 @@
 from os import path, mkdir
 import train
-import csv
 import numpy as np
 import pandas as pd
 from copy import deepcopy
@@ -19,20 +18,18 @@ nproc = 1
 if not path.exists('results'):
     mkdir('results')
 
-with open(train.args.result_file, 'w') as fp:
-    writer = csv.DictWriter(fp, train.results.keys())
-    writer.writeheader()
+train.write_csv_header()
 
 tasks = []
 
-id = 0
+task_id = 0
 for seed in range(100,105):
     for set_size_range in [[10,100], [100,1000]]:
         for max_set_new_size_train in set_size_range:
             for max_set_new_size_test in set_size_range:
                 for new_sets_number_train in [int(np.ceil(float(set_size_range[1])/max_set_new_size_train))]:
                     for new_sets_number_test in [int(np.ceil(float(set_size_range[1]) / max_set_new_size_test))]:
-                        id += 1
+                        task_id += 1
                         new_args = deepcopy(train.args)
                         new_args.id = id
                         new_args.max_set_new_size_train = max_set_new_size_train
